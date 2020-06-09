3753681e9f797406babbf6ff7a41ac79f0149cae
elitmor <elitmor@gmail.com>
2020-06-09 12:39:08 +0300

    added header

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $('.header__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+    });
+});
