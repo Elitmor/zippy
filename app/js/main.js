4ae6edfa4c527a9dfb7e265283d46cdfba4c9426
elitmor <elitmor@gmail.com>
2020-06-12 11:19:22 +0300

    added search and blog

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,19 @@ $(function() {
         arrows: false,
         dots: true,
     });
+
+    $('.about__slider-inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider__nav'
+    });
+    $('.slider__nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.about__slider-inner',
+
+    });
+
 });
