44ddb5b8febaab5c012b7ab9a6fb507226b199f0
elitmor <elitmor@gmail.com>
2020-06-15 15:44:40 +0300

    added adaptive and fix some section

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,10 @@ $(function() {
         slidesToScroll: 1,
         arrows: false,
         dots: true,
+
     });
+
+
 
     $('.about__slider-inner').slick({
         slidesToShow: 1,
@@ -14,7 +17,10 @@ $(function() {
         arrows: false,
         fade: true,
         asNavFor: '.slider__nav'
+
     });
+
+
     $('.slider__nav').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -23,3 +29,16 @@ $(function() {
     });
 
 });
+
+$('.header__menu-btn').on('click', function() {
+    $('.menu').slideToggle();
+})
+
+// responsive: [{
+//     breakpoint: 900,
+//     settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         arrows: false,
+//     }
+// }]
