27fe5b302171f12ffb198d6d011fae452a11f539
elitmor <elitmor@gmail.com>
2020-06-16 21:03:44 +0300

    fix bag

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,12 @@ $(function() {
         slidesToScroll: 1,
         arrows: false,
         dots: true,
-
+        fade: true,
+        speed: 800,
+        infinite: true,
+        cssEase: 'ease-in-out',
+        touchThreshold: 100,
+        appendDots: '.header__inner-content',
     });
 
 
@@ -33,6 +38,41 @@ $(function() {
 $('.header__menu-btn').on('click', function() {
     $('.menu').slideToggle();
 })
+
+function GoUrl(url) {
+    location.href = url;
+}
+
+function initialize() {
+    var myLatlng = new google.maps.LatLng(53.3333, -3.08333),
+        mapOptions = {
+            zoom: 11,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions),
+        contentString = 'Здесь должен быть какой-то адрес',
+        infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 500
+        });
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+    });
+
+    google.maps.event.addDomListener(window, "resize", function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
 
 // responsive: [{
 //     breakpoint: 900,
